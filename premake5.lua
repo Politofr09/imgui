20891368e650ec3aaa0422e6126476ab700b4900
Politofr09 <deshidratatorpa@gmail.com>
2025-02-05 20:49:37 +0100

    Update on build script; creating submodule

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,47 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
 
-	targetdir ("bin/%{prj.name}")
-	objdir ("bin-obj/%{prj.name}")
+	targetdir ("bin/%{cfg.buildcfg}")
+	objdir ("bin-obj/%{cfg.buildcfg}")
+
+	includedirs
+	{
+		"./",
+		"backends",
+		
+		-- Necessary for vulkan
+		"%{VULKAN_SDK}/Include/"
+		-- "%{LibraryDir.Vulkan}"
+	}
 
 	files
 	{
 		"imconfig.h",
 		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
 		"imgui_internal.h",
-		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+
+		-- Selected backend: Vulkan
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_vulkan.h",
+		"backends/imgui_impl_vulkan.cpp"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
 		staticruntime "On"
 
 	filter "system:linux"
-		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
 		staticruntime "On"
 
 	filter "configurations:Debug"
